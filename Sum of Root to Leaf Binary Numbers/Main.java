55e5cd8f400523789b1d084ed35aa0fa2ef99615
Saniya Ahemad <167697355+saniyaahemad12@users.noreply.github.com>
2026-02-24 19:55:22 +0530

    Add files via upload

---
+++ Sum of Root to Leaf Binary Numbers/Main.java
@@ -0,0 +1,21 @@
+ */
+class Solution {
+
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    private int dfs(TreeNode node, int sum) {
+        if (node == null) {
+            return 0;
+        }
+
+        sum = (sum << 1) + node.val;
+
+        if (node.left == null && node.right == null) {
+            return sum;
+        }
+
+        return dfs(node.left, sum) + dfs(node.right, sum);
+    }
+}
